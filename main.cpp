94746cdfbb70918e0b5d96caecc64fa2af42525d
ruka <115062996+merutao@users.noreply.github.com>
2023-11-07 17:22:57 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -1,58 +1,69 @@
+//ヘッダファイルのインクルード
 #include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
 
-//template<typename T>
+typedef void (*newType)(int*, int*);
 
-//T Min(T a, T b)
-//{
-//	//aが大きい時bを返す
-//	if (a > b)
-//	{
-//		return static_cast<T> (b);
-//	}
-//
-//	//bが大きい時aを返す
-//	if (a < b)
-//	{
-//		return static_cast<T> (a);
-//	}
-//}
-//
-//template<>
-//char Min<char>(char a, char b)
-//{
-//	printf("数字以外は代入できません\n");
-//
-//	return 0;
-//}
-
-int Recursive(int time, int all, int m)
- {
-	if (time <= 0)  
+//結果  奇数か偶数か
+void DispResult(int* number, int* answer)
+{
+	if (*number == 1)
 	{
-		return(all);
+		if (*answer == 1 || *answer == 3 || *answer == 5)
+		{
+			printf("奇数\n");
+		}
+		else
+		{
+			printf("×\n");
+		}
 	}
-	return Recursive(time - 1, all += m, m * 2 - 50);
+	
+
+	if (*number == 0)
+	{
+		if (*answer == 2 || *answer == 4 || *answer == 6)
+		{
+			printf("偶数\n");
+		}
+		else
+		{
+			printf("×");
+		}
+	}
+	
+	
 }
+
+//結果まで3秒待つ  コールバック
+void setTimeout(newType calc, int second,int number ,int answer)
+{
+	Sleep(second * 180);
+
+	calc(&number, &answer);
+
+}
+
 
 int main()
 {
-	/*char a = 'a';
-	char b = 'b';
+	srand((unsigned int)time(NULL));
 
-	printf("%d\n", Min<int>(200, 100));
-	printf("%f\n", Min<float>(20, 100));
-	printf("%f\n", Min<double>(200, 100));
+	newType calc;
 
-	Min<char>(a, b);*/
+	calc = DispResult;
 
-	int all = 0;
-	int time = 8;	//時間
-	int m = 100;	//階乗する値
-	int j = 1072;	//時給
-	int result;		//計算結果
+	int number = 0;
+	int answer;
 
-	result = Recursive(time, all, m);
-	printf("一般的な賃金体系%d : 再帰的な賃金体系%d\n", time * j, result);
+	printf("偶数なら０を、奇数なら１を\n");
+	scanf_s("%d", &number);
+
+	answer = 1 + rand() % 6;
+
+	setTimeout(calc, 3, number, answer);
 
 	return(0);
 }
