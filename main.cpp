1e63bc1d636f00074670abcbee1478b222b07e0b
ruka <115062996+merutao@users.noreply.github.com>
2024-02-13 20:09:37 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -4,51 +4,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <functional>
+#include <thread>
+
+
+void Thread1() {
+	printf("thread1\n");
+}
+
+void Thread2() {
+	printf("thread2\n");
+}
+
+void Thread3() {
+	printf("thread3\n");
+}
 
 int main()
 {
-	srand((unsigned int)time(NULL));
+	std::thread thread1(Thread1);
+	std::thread thread2(Thread2);
+	std::thread thread3(Thread3);
 
-	int num;
-	int select = 0;
-
-	printf("半なら0を、丁なら1を入力\n");
-	scanf_s("%d", &select);
-
-	num = 1 + rand() % 6;
-
-	std::function<void(void)>lottery = [=]()
-		{
-			printf("サイコロの目：%d\n", num);
-
-			if (select == 0) {
-				if (num == 1 || num == 3 || num == 5) {
-					printf("半");
-				}
-				else {
-					printf("残念");
-				}
-			}
-
-			if (select == 1) {
-				if (num == 0 || num == 2 || num == 4 || num == 6) {
-					printf("丁");
-				}
-				else {
-					printf("残念");
-				}
-			}
-
-		};
-
-	std::function<void(int)> setTimeout = [=](int second) {
-		Sleep(second * 1000);
-
-		lottery();
-
-		};
-
-	setTimeout(3);
+	thread1.join();
+	thread2.join();
+	thread3.join();
 
 	return(0);
 }
